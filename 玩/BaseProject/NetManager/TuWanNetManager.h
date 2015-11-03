6e210a1d02761385901221e6d2366db8eb81b769
dangyt <tarena@dangyuting.local>
2015-11-03 14:53:30 +0800

    使用枚举变量，来决定不同的请求地址即可

--- 玩/BaseProject/NetManager/TuWanNetManager.h
+++ 玩/BaseProject/NetManager/TuWanNetManager.h
@@ -8,13 +8,24 @@
 
 #import "BaseNetManager.h"
 
-//只要公用一个解析类的请求，就可以合起来写，只需要使用枚举变量，来决定不同的请求地址即可
+//只要是公用一个解析类的请求，就可以合起来写，只需要使用枚举变量，来决定不同的请求地址即可
 //如果不会  可以参考汽车之家接口
 
 typedef NS_ENUM(NSUInteger, InfoType) {
     InfoTypeTop,        //头条
-    InfoTypeSingle,    //独家
-    //
+    InfoTypeSingle,     //独家
+    InfoTypeAnHei,      //暗黑3
+    InfoTypeMoShou,     //魔兽
+    InfoTypeFengBao,    //风暴
+    InfoTypeLuShi,      //炉石
+    InfoTypeXingJi,     //星际2
+    InfoTypeShouWang,   //守望
+    InfoTypeTuPian,     //图片
+    InfoTypeShiPin,     //视频
+    InfoTypeGongLue,    //攻略
+    InfoTypeHuanHua,    //幻化
+    InfoTypeQuWen,      //趣闻
+    InfoTypeMeiNv,      //美女
 };
 
 @interface TuWanNetManager : BaseNetManager
